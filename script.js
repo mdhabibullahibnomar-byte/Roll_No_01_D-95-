2b86b5e94974f9dc579d1ceab18a31511a7dec68
mdhabibullahibnomar-byte <mdhabibullahibnomar@gmail.com>
2026-08-17 21:14:33 +0600

    Update data

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-console.log("I am Connected")
-const h1 = document.getElementsByTagName("h1")
-console.log(h1)
-for (let i = 0; i<h1.length; i++){
-    h1[i].style.color = "red";
-    h1[i].style.
-    backgroundColor = "yellow";
-}
-const para = document.getElementById("para")
-console.log(para)
-para.style.color = "green"
-console.log(para.innerText);
-const div = document.getElementById("div")
-console.log(div.innerText)
-console.log(div.innerHTML)
-const obj = [
-    {
-        name : "John",
-        city : "Dallas"
-    },
-    {
-        name : "Jany",
-        city : "Dhaka"
-    }
-];
-const userHTML = document.getElementById("user");
-obj.forEach((user) => {
-    userHTML.innerHTML += `
-    <h3> ${user.name} </h3>
-    <address> ${user.city} </address>`
-})
-for(let i=0;i<99;i++){
-fetch("https://jsonplaceholder.typicode.com/users")
-.then((res => res.json()))
-.then(data => {
-    data.forEach((user) =>{
-        userHTML.innerHTML+=`
-        <h1>${user.name}</h1>
-        <email>${user.email} </email>
-        <address>${user.address.street}, ${user.address.city}, ${user.address.zipcode}</address>`
-    })
-});
-}
+const p = document.getElementById("p");
+const commcentsHTML = document.getElementById("c");
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res => res.json()))
+    .then(data => {
+        data.forEach((posts) => {
+            p.innerHTML += `
+        <h3>User's ID: ${posts.userId}</h3>
+        <p>Posts ID: ${posts.id}</p>
+        <h5>Title: ${posts.title}</h5>
+        <p>${posts.body}</p> `
+        })
+    });
+fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((res => res.json()))
+    .then(data => {
+        data.forEach((comments) => {
+            c.innerHTML += `
+        <h3>Post's ID: ${comments.postId}</h3>
+        <p>Comments ID: ${comments.id}</p>
+        <p>email: ${comments.email}</p>
+        <p>${comments.body}</p> `
+        })
+    });
